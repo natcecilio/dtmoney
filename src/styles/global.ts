00183203aa9cb740ac0be6ec1d9f6ac8a94eb123
natcecilio <tatafly2009@hotmail.com>
2022-01-04 22:31:08 -0300

    crianção modal e estilização btns

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,17 +2,14 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   :root{
-    --background: #f8f2f5;
+    --background: #f0f2f5;
     --red: #e52e4d;
     --green: #33cc95;  
     --blue: #5429cc;
-
     --blue-light: #6933ff;
 
     --text-title: #363f5f;
     --text-body: #969cb3;
-
-    --background: #F0F2F5;
     --shape: #ffffff;
   }
 
@@ -36,7 +33,7 @@ export const GlobalStyle = createGlobalStyle`
   // REM = 1rem = 16px = font-size
 
   body {
-    background: var(---background);
+    background: var(--background);
     -webkit-font-smoothing: antialiased;
   }
 
@@ -57,6 +54,46 @@ export const GlobalStyle = createGlobalStyle`
     opacity: 0.6;
     cursor: not-allowed;
   }
+
+  .react-modal-overlay{
+    background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content{
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.24rem;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
+  }
+
+
+
 
 `
 
